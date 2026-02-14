d802e06b067d6bda3c4d62031bfb8bc8f8765817
horatiolyons <lyonshoratio@gmail.com>
2026-02-14 21:09:33 +0800

    Refactor getRandomNumber and moveNonButton functions

--- script.js
+++ script.js
@@ -32,13 +32,8 @@ function moveNonButton() {
 function initialize() {
     document.getElementById('nonButton').addEventListener('mouseenter', moveNonButton);
     window.addEventListener('resize', moveNonButton);
-    document.getElementById('nonButton').addEventListener('mouseenter', function() {
-        img.src = 'img/cats-sad.gif';
-    });
-    document.getElementById('ouiButton').addEventListener('mouseenter', function() {
-        img.src = 'img/love-cat.gif';
-    });
 }
 
 const img = document.querySelector('img');
 initialize();
+
